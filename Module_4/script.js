a1c8db74d886c92b920c732e158306a3ed56c191
DmytroBon <105019007+DmytroBon@users.noreply.github.com>
2022-11-20 19:25:45 +0200

    4

--- Module_4/script.js
+++ Module_4/script.js
@@ -1,28 +1,18 @@
-/*
-Solution of assignment 4:
+(function () {
 
-Expected output:
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-Hello Yaakov
-Good Bye John
-Good Bye Jen
-Good Bye Jason
-Hello Paul
-Hello Frank
-Hello Larry
-Hello Paula
-Hello Laura
-Good Bye Jim
-*/
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
 
-(function() {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    for (var i = 0; i < names.length; i++) {
-        var firstLetter = names[i].charAt(0).toLowerCase();
-        if (firstLetter === 'j') {
-            byeSpeaker.speak(names[i]);
-        } else {
-            helloSpeaker.speak(names[i]);
-        }
+    if (firstLetter === 'j') {
+      byeSpeaker(names[i]);
     }
+    else {
+      helloSpeaker(names[i]);
+    }
+  }
 })();
+
+
+
